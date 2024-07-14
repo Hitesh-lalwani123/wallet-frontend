a36dc98b0178e43f74a65fb8982409248484f7c5
Hitesh lalwani <georgelook2800@gmail.com>
2024-07-14 19:47:47 +0530

    Added mobile responsiveness to card comp

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 
 const Card = ({children}) => {
   return (
-    <div className="shadow-sm shadow-black w-1/3 mx-auto flex flex-col my-4 p-2">
+    <div className="shadow-sm shadow-black w-11/12 sm:w-1/2 md:w-1/3 mx-auto flex flex-col my-4 p-2">
         {children}
     </div>
   )
